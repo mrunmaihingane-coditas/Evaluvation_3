a1411b49e9d35432a6315436358cefa4741fc37c
mrunmaihingane-coditas <124657592+mrunmaihingane-coditas@users.noreply.github.com>
2023-02-18 16:09:35 +0530

    Update Q1_perimeter.java

--- Q1_perimeter.java
+++ Q1_perimeter.java
@@ -1,30 +1,60 @@
-class Rectangle{
+class Rectangle {
+    private int l;
+    private int b;
+    private int h;
 
-    Rectangle(int l, int b){
+    public int getL() {
+        return l;
+    }
 
+    public int getB() {
+        return b;
+    }
+
+    public void setL(int l) {
+        this.l = l;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getareaOfRectangle(){
+       return  this.getB()*this.getL();
+    }
+    public int getPerimeterOfRectangel(){
+        return 2*this.getH()+(this.getL()*this.getB());
     }
 
 }
-class calculation extends Rectangle{
-    calculation(int l, int b) {
-        super(l, b);
-        System.out.println("Perimeter: "+ 2*(l*b));
-        System.out.println("area: " + l*b);
-    }
-
-
-}
-
-
-
 
 public class Q1_perimeter {
     public static void main(String[] args) {
-               Rectangle rectangle = new calculation(1,4);
+    Rectangle rectangle = new Rectangle();
+    rectangle.setL(2);
+    rectangle.setB(3);
+    rectangle.setH(6);
+
+        System.out.println("This is area of rectangle: "+rectangle.getareaOfRectangle());
+        System.out.println("This is perimeter of rectangle: "+rectangle.getPerimeterOfRectangel());
+
+
+
 
     }
 }
-//Perimeter: 8
-//area: 4
+
+
+//This is area of rectangle: 6
+//This is perimeter of rectangle: 18
 //
 //Process finished with exit code 0
+// Correction done...
